4c4f3e2c8c35c06d34ba66c97d34a4ca89488271
Russel Rai <leon.amorphy@googlemail.com>
2022-08-08 23:11:10 +0100

    initial commit

---
+++ part0/readme.md
@@ -0,0 +1,1 @@
+part 0 of the course which is just basic reading and explaining.
---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,60 @@
+const Header = ({course}) => {
+  return <h1>{course}</h1>;
+};
+
+const Content = ({ parts }) => {
+    return (
+        parts.map((part) => {
+            return (
+                <Part part={part} />
+            )
+        })
+    )
+};
+
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  );
+};
+
+const Total = ({ parts }) => {
+  let total = 0;
+  parts.forEach((part) => {
+    total += part.exercises;
+  });
+
+  return <p>Number of exercises {total}</p>;
+};
+
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+
+export default App;
