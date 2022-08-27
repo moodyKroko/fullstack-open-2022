833f6a564e8089f1c9fe33e68a1f76e14a3a3801
Russel Rai <leon.amorphy@googlemail.com>
2022-08-27 02:02:38 +0100

    part 2.14 done

--- part2/countryinfo/src/components/Weather.js
+++ part2/countryinfo/src/components/Weather.js
@@ -1,11 +1,6 @@
-import axios from 'axios'
 import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
-/**
- * weather API respons >
- * get current.temp, current.wind_speed, current.weather[0].id, current.weather[0].icon
- * https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
- */
 const baseUrl = 'https://api.openweathermap.org/data/2.5'
 const API_KEY = process.env.REACT_APP_API_KEY
 
@@ -14,32 +9,28 @@ const Weather = ({ city }) => {
 
   useEffect(() => {
     axios
-      .get(`${baseUrl}/weather?q=${city}&appid=${API_KEY}`)
+      .get(`${baseUrl}/weather?q=${city}&appid=${API_KEY}&units=metric`)
       .then((response) => {
+        console.log('weather data loaded')
         setWeather(response.data)
-      })
-      .catch((error) => {
-        if (error.response) {
-          // The request was made and the server responded with a status code
-          // that falls out of the range of 2xx
-          console.log(error.response.status)
-          console.log(error.response.data.message)
-        } else if (error.request) {
-          // The request was made but no response was received
-          console.log(error.request)
-        } else {
-          // Something happened in setting up the request that triggered an Error
-          console.log('Error', error.message)
-        }
       })
   }, [city])
 
+  if (!weather) return
+
+  const cityTemp = weather.main.temp
+  const iconCode = weather.weather[0].icon
+
   return (
-    <>
-      <h2>Weather in Helsinki</h2>
-      <p>{city}</p>
-      temperature
-    </>
+    <div>
+      <h2>Weather in {city}</h2>
+      <div>temperature {cityTemp} Celcius</div>
+      <img
+        src={`https://openweathermap.org/img/wn/${iconCode}@2x.png`}
+        alt='Weather icons'
+      />
+      <div>wind {weather.wind.speed} m/s</div>
+    </div>
   )
 }
 
