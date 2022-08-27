706257fa43406d1d88aee809e391c96e72625950
Russel Rai <leon.amorphy@googlemail.com>
2022-08-27 14:28:47 +0100

    fixed structure

--- part2/countryinfo/src/components/Weather.js
+++ part2/countryinfo/src/components/Weather.js
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
-const baseUrl = 'https://api.openweathermap.org/data/2.5'
-const API_KEY = process.env.REACT_APP_API_KEY
-
 const Weather = ({ city }) => {
   const [weather, setWeather] = useState(null)
 
   useEffect(() => {
+    const baseUrl = 'https://api.openweathermap.org/data/2.5'
+    const API_KEY = process.env.REACT_APP_API_KEY
+
     axios
       .get(`${baseUrl}/weather?q=${city}&appid=${API_KEY}&units=metric`)
       .then((response) => {
@@ -20,16 +20,19 @@ const Weather = ({ city }) => {
 
   const cityTemp = weather.main.temp
   const iconCode = weather.weather[0].icon
+  const windSpeed = weather.wind.speed
+
+  const iconURl = 'https://openweathermap.org/img/wn'
 
   return (
     <div>
       <h2>Weather in {city}</h2>
       <div>temperature {cityTemp} Celcius</div>
       <img
-        src={`https://openweathermap.org/img/wn/${iconCode}@2x.png`}
+        src={`${iconURl}/${iconCode}@2x.png`}
         alt='Weather icons'
       />
-      <div>wind {weather.wind.speed} m/s</div>
+      <div>wind {windSpeed} m/s</div>
     </div>
   )
 }
