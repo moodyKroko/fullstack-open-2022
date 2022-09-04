4d36070b81c9acd084cf6e2737895952be8fd1a5
Russel Rai <leon.amorphy@googlemail.com>
2022-09-04 23:09:47 +0100

    add mongoose dependency and mongo.js file

---
+++ part3/phonebook/mongo.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://fullstack:${password}@cluster0.pb4sqgz.mongodb.net/phonebookApp?retryWrites=true&w=majority`
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const person = new Person({
+  name: 'Test',
+  number: '15-48-7845698',
+})
+
+if (true) {
+  person.save().then((result) => {
+    console.log('person saved')
+    mongoose.connection.close()
+  })
+}
+
+Person.find({}).then((result) => {
+  result.forEach((person) => {
+    console.log(person)
+  })
+  mongoose.connection.close()
+})
