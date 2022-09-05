93d76e41ac21ccabef2c764fb7e099408f47290d
Russel Rai <leon.amorphy@googlemail.com>
2022-09-05 14:48:00 +0100

    ex 3.13-3.14 done
    create person model for saving to database
    
    install dotenv
    
    delete hardcoded data
    
    create REST api test files
    
    added http request to test
    
    organised imports and imported dependencies
    
    add .env file to ignore list
    
    changed console log to not show url
    
    add get, getById, info http methods calls
    
    remove generateId method
    
    remove find existing person functionality
    
    add post method to database, added tests
    
    add delete by id + api test, ex 3.13-3.14 done
    ex 3.13-3.14 compelete

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,31 +1,10 @@
 const express = require('express')
 const logger = require('morgan')
-
 const app = express()
 const cors = require('cors')
 
-let persons = [
-  {
-    id: 1,
-    name: 'Arto Hellas',
-    number: '040-123456',
-  },
-  {
-    id: 2,
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-  },
-  {
-    id: 3,
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-  },
-  {
-    id: 4,
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-  },
-]
+require('dotenv').config()
+const Person = require('./models/person')
 
 app.use(express.json())
 
@@ -46,66 +25,61 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then((persons) => {
+    response.json(persons)
+  })
 })
 
+const getTotalDocCount = async () => {
+  const count = await Person.countDocuments()
+  return count
+}
+
 app.get('/info', (request, response) => {
-  response.send(`Phonebook has info for ${persons.length} people
-  ${new Date()}`)
+  getTotalDocCount().then((count) => {
+    response.send(`Phonebook has info for ${count} people\n${new Date()}`)
+  })
 })
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find((person) => person.id === id)
-
-  if (person) {
-    return response.json(person)
-  }
-
-  response.status(404).end()
+  Person.findById(request.params.id)
+    .then((person) => {
+      response.json(person)
+    })
+    .catch((error) => {
+      response.status(404).end()
+    })
 })
-
-const generateID = () => {
-  const maxId = persons.length > 0 ? Math.floor(Math.random() * 500) : 0
-  return maxId
-}
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
 
+  const returnErrResponse = (statusCode, message) => {
+    return response.status(statusCode).json({
+      error: message,
+    })
+  }
+
   if (!body.name) {
-    return response.status(400).json({
-      error: 'name missing',
-    })
+    returnErrResponse(400, 'name missing')
   } else if (!body.number) {
-    return response.status(400).json({
-      error: 'number missing',
-    })
+    returnErrResponse(400, 'number missing')
   }
 
-  const existing = persons.find((person) => person.name === body.name)
-
-  if (existing) {
-    return response.status(409).json({
-      error: 'name must be unique',
-    })
-  }
-
-  const person = {
+  const person = new Person({
     name: body.name,
     number: body.number,
-    id: generateID(),
-  }
+  })
 
-  persons = persons.concat(person)
-  response.json(person)
+  person.save().then((savedPerson) => {
+    response.json(savedPerson)
+  })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter((person) => person.id !== id)
-
-  response.status(200).end()
+  Person.findByIdAndDelete(request.params.id).then(() => {
+    response.status(200).end()
+  })
 })
 
 const PORT = process.env.PORT || 3001
